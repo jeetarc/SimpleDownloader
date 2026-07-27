44da32131481a4ada14754aed8e770e51daaf3c1
Jeet <jeetugg04@gmail.com>
2026-07-27 22:02:23 +0530

    Enhance MimeType enum with FROM_NAME mode
    Added FROM_NAME mode to MimeType enum and updated comments.

--- simpledownloader/src/main/java/com/jeet/simpledownloader/MimeType.java
+++ simpledownloader/src/main/java/com/jeet/simpledownloader/MimeType.java
@@ -1,11 +1,25 @@
 package com.jeet.simpledownloader;
 
 /*
- * Copyright (c) 2026 Jeet Jati, under jeetarc.
- *
- * This source code is part of SimpleDownloader.
+* Copyright (c) 2026 Jeet / Jeetarc.
+*
+* This source code is part of SimpleDownloader.
+*/
+
+/**
+ * Use to get automatically resolved MIME type for an output file.
+ * Tow modes {@code MimeType.AUTO} and {@code MimeType.FROM_NAME}
  */
- 
 public enum MimeType {
-	AUTO
+	AUTO, FROM_NAME;
+	
+	static MimeType fromDatabaseValue(String value) {
+		if (value == null) return null;
+		
+		try {
+			return valueOf(value);
+		} catch (IllegalArgumentException ignored) {
+			return null;
+		}
+	}
 }
