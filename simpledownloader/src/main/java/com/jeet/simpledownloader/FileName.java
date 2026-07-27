39de62e3e048d1e9da3adc907baaa26e525cda6a
Jeet <jeetugg04@gmail.com>
2026-07-27 21:57:52 +0530

    Add FileName.TIME_BASED
    Updated copyright notice and corrected enum value from TIME_MILLIS to TIME_BASED.

--- simpledownloader/src/main/java/com/jeet/simpledownloader/FileName.java
+++ simpledownloader/src/main/java/com/jeet/simpledownloader/FileName.java
@@ -1,11 +1,24 @@
 package com.jeet.simpledownloader;
 
 /*
- * Copyright (c) 2026 Jeet Jati, under jeetarc.
- *
- * This source code is part of SimpleDownloader.
+* Copyright (c) 2026 Jeet / Jeetarc.
+*
+* This source code is part of SimpleDownloader.
+*/
+
+/**
+ * Use to get automatically resolved name for an output file.
+ * Tow modes {@code FileName.AUTO} and {@code FileName.TIME_BASED}
  */
- 
 public enum FileName {
-	AUTO, TIME_MILLIS
+	AUTO, TIME_BASED;
+	
+	static FileName fromDatabaseValue(String value) {
+		if (value == null) return null;
+		try {
+			return valueOf(value);
+		} catch (IllegalArgumentException ignored) {
+			return null;
+		}
+	}
 }
