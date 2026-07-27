4ec92cb6ad1ceb012c8908b219cfbe88b6769103
Jeet <jeetugg04@gmail.com>
2026-07-27 22:09:29 +0530

    Enhance Priority.java
    Added methods to convert from code and database value to Priority.

--- simpledownloader/src/main/java/com/jeet/simpledownloader/Priority.java
+++ simpledownloader/src/main/java/com/jeet/simpledownloader/Priority.java
@@ -1,20 +1,44 @@
 package com.jeet.simpledownloader;
 
 /*
- * Copyright (c) 2026 Jeet Jati, under jeetarc.
+ * Copyright (c) 2026 Jeet / Jeetarc.
  *
  * This source code is part of SimpleDownloader.
  */
- 
+
+/**
+ * The priority of a download task in the queue.
+ *
+ * <p>Tasks with higher priority are start download before lower priority tasks. 
+ * (eg. NEXT > HIGH > NORMAL > LOW) </p>
+ */
 public enum Priority {
 	NEXT(4), HIGH(3), NORMAL(2), LOW(1);
 	final int weight;
 	
-	Priority(int weight) { 
-		this.weight = weight; 
+	Priority(int weight) {
+		this.weight = weight;
 	}
 	
 	public int getWeight() {
-		return weight; 
+		return weight;
+	}
+	
+	static Priority fromCode(int code) {
+		for (Priority priority : values()) {
+			if (priority.weight == code) return priority;
+		}
+		return NORMAL;
+	}
+	
+	static Priority fromDatabaseValue(String value) {
+		if (value == null || value.length() <= 0) return NORMAL;
+		try {
+			return fromCode(Integer.parseInt(value));
+		} catch (Exception ignored) {}
+		try {
+			return valueOf(value);
+		} catch (Exception ignored) {}
+		return NORMAL;
 	}
 }
