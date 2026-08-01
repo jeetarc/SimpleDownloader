4e513c0f073d2026481329e9017c6647cd33fab0
Jeet <jeetugg04@gmail.com>
2026-08-01 20:31:37 +0530

    Enhance done method error handling
    Added error handling in done method.

--- simpledownloader/src/main/java/com/jeet/simpledownloader/DownloadFutureTask.java
+++ simpledownloader/src/main/java/com/jeet/simpledownloader/DownloadFutureTask.java
@@ -1,12 +1,14 @@
 package com.jeet.simpledownloader;
 
 /*
- * Copyright (c) 2026 Jeet / Jeetarc.
- *
- * This source code is part of SimpleDownloader.
- */
- 
+* Copyright (c) 2026 Jeet / under Jeetarc.
+*
+* This source code is part of SimpleDownloader.
+*/
+
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
 
 class DownloadFutureTask extends FutureTask<Object> implements Comparable<DownloadFutureTask> {
 	private final DownloadRunnable runnable;
@@ -19,6 +21,29 @@ class DownloadFutureTask extends FutureTask<Object> implements Comparable<Downlo
 	@Override
 	public int compareTo(DownloadFutureTask other) {
 		return runnable.compareTo(other.runnable);
+	}
+	
+	@Override
+	protected void done() {
+		if (isCancelled()) return;
+		
+		try {
+			get();
+			
+		} catch (CancellationException ignored) {
+			// normal cancellation, no problem! 
+			
+		} catch (InterruptedException error) {
+			Thread.currentThread().interrupt();
+			throw new RuntimeException(error);
+			
+		} catch (ExecutionException error) {
+			Throwable cause = error.getCause();
+			if (cause instanceof Error) throw (Error) cause;
+			if (cause instanceof RuntimeException) throw (RuntimeException) cause;
+            
+			throw new RuntimeException(cause);
+		}
 	}
 	
 	DownloadTask getTask() {
