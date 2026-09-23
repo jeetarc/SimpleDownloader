2dee80e9772cec4dd61b31e668a7e3d368934ecd
Jeet <jeetugg04@gmail.com>
2026-09-23 20:38:04 +0530

    Remove semicolon from setDeleteOnRemoval method

--- app/src/main/java/app/jeetarc/simpledownloader/App.kt
+++ app/src/main/java/app/jeetarc/simpledownloader/App.kt
@@ -24,7 +24,7 @@ class App : Application() {
 		.enableSorting(true)
 		.build()
 
-		downloader.setDeleteOnRemoval(true);
+		downloader.setDeleteOnRemoval(true)
 	}
 	
 	fun getDownloader(): SimpleDownloader {
