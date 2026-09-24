1d874f1238687daea580915c0e82b4eeeea61425
Jeet <jeetugg04@gmail.com>
2026-09-24 20:48:16 +0530

    Add copyright notice and update imports in DownloadFragment
    Updated DownloadFragment to include a copyright notice and modified imports for compatibility.

--- app/src/main/java/app/jeetarc/simpledownloader/DownloadFragment.kt
+++ app/src/main/java/app/jeetarc/simpledownloader/DownloadFragment.kt
@@ -1,5 +1,11 @@
 package app.jeetarc.simpledownloader
 
+/*
+* Copyright (c) 2026 Jeet / Jeetarc.
+*
+* This source code is part of SimpleDownloader.
+*/
+
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.drawable.GradientDrawable
@@ -15,7 +21,7 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager.widget.ViewPager
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -235,7 +241,7 @@ class DownloadFragment : Fragment() {
 	}
 	
 	fun goToTaskListPage() {
-		val viewPager = requireActivity().findViewById<ViewPager2>(R.id.viewPagerNav)
+		val viewPager = requireActivity().findViewById<ViewPager>(R.id.viewPagerNav)
 		viewPager.setCurrentItem(1, true)
 	}
 	
