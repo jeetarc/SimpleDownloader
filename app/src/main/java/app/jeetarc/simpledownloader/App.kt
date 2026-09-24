72d730cba1c5c07aa939957ac5b5aa3320ef0ce0
Jeet <jeetugg04@gmail.com>
2026-09-24 20:45:04 +0530

    Enhance App with Listener callbacks
    Added copyright notice and toast message functionality for download events.

--- app/src/main/java/app/jeetarc/simpledownloader/App.kt
+++ app/src/main/java/app/jeetarc/simpledownloader/App.kt
@@ -1,6 +1,16 @@
 package app.jeetarc.simpledownloader;
 
+/*
+* Copyright (c) 2026 Jeet / Jeetarc.
+*
+* This source code is part of SimpleDownloader.
+*/
+
 import android.app.Application
+import android.net.Uri
+import android.widget.Toast
+
+import com.jeet.simpledownloader.DownloadTask
 import com.jeet.simpledownloader.SimpleDownloader
 
 class App : Application() {
@@ -20,12 +30,25 @@ class App : Application() {
 		.setAutoRestore(true)
 		.enableSorting(true)
 		.build()
-
+        
 		downloader.setDeleteOnRemoval(true)
+        
+		downloader.addListener(object : SimpleDownloader.Listener() {
+			override fun onComplete(outputUri: Uri, task: DownloadTask) {
+				showToast("Download complete!")
+			}
+			
+			override fun onError(outputUri: Uri, err: Exception, task: DownloadTask) {
+                showToast("Download failed: " + err.message)
+			}
+		})
 	}
 	
 	fun getDownloader(): SimpleDownloader {
 		return downloader
 	}
 	
+    private fun showToast(message: String) {
+		Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+	}
 }
